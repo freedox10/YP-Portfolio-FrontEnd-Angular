f046cbe8cced3c7be79dd950562f62175e3f7a97
freedox10 <alfre2636@yahoo.com.ar>
2023-04-25 16:29:30 -0300

    Creación de componentes Header y Banner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    HeaderComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
